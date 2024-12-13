b43c7a3c5e9919e60e97d6403456401c23108d45
Nicholas Landers <nicholaslanders1234@gmail.com>
2024-12-12 20:24:57 -0500

    Switched from jest to vitest, as it allows me to use import/export. Wrote some unit tests for the compute file.

---
+++ tests/compute.test.ts
@@ -0,0 +1,18 @@
+import { compute } from "../src/compute.js";
+import { test, expect } from "vitest";
+
+test("Addition Test", () => {
+  expect(compute("10", "20", "+")).toBe("30");
+});
+
+test("Subtraction Test", () => {
+  expect(compute("10", "20", "-")).toBe("-10");
+});
+
+test("Multiplying Test", () => {
+  expect(compute("10", "20", "x")).toBe("200");
+});
+
+test("Dividing Test", () => {
+  expect(compute("10", "20", "÷")).toBe("0.5");
+});
