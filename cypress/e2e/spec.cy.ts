09c7d03306baf58948f9823b02dd10b9bbec730f
Nicholas Landers <nicholaslanders1234@gmail.com>
2024-12-15 19:23:35 -0500

    Wrote up some test cases for the UI. This is definitely a testing tool I will use in the future. Also changed the placeholder value in the HTML to line up with the testing cases.

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,32 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+/// <reference types="cypress"/>
+
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:5500/public");
+  });
+
+  it("initializes as 0", () => {
+    cy.get("#display").should("have.value", "0");
+  });
+
+  it("should have all the buttons visible", () => {
+    cy.get(".number").each((number) => {
+      cy.wrap(number).should("be.visible");
+    });
+  });
+
+  it("Puts a number on the screen when a button is pressed", () => {
+    cy.get(".number").each((number) => {
+      let curNum: string = number.text();
+      cy.get(number).click();
+      cy.get("#display").should("have.value", curNum);
+      cy.get("#C").click();
+    });
+  });
+
+  it("Should clear numbers when pressing C", () => {
+    cy.get("button").contains("5").click();
+    cy.get("#C").click();
+    cy.get("#display").should("have.value", "0");
+  });
+});
